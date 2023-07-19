9bc7ecd136ffb7647202ff4d6792199bac6bbdc2
AddankiLeelaPrasanna <137883522+AddankiLeelaPrasanna@users.noreply.github.com>
2023-07-19 10:33:53 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+function tom(){
+    document.getElementById("cartoon").src="https://i.pinimg.com/474x/92/a6/2f/92a62f0221f58fe503a15fcb13f5c107--tom-jerry-movies-.jpg";
+}
+function jerry(){
+    document.getElementById("cartoon").src="https://i.pinimg.com/550x/85/58/29/85582987f7125e4868fdcb28661e934f.jpg";
+}
+function dora(){
+    document.getElementById("cartoon").src="https://i.pinimg.com/736x/ea/4c/7e/ea4c7e71fbcdc7d776e7163f337ec4fe--dora-the-explorer-cartoon-characters.jpg";
+}
